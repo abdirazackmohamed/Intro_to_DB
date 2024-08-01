af8f8751555e244fd9cddf60b9aa56958cf5bd80
abdirazackmohamed <abdirazackmohamad@gmail.com>
2024-08-01 20:07:55 +0300

    adding create db into code

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,5 +1,4 @@
-CREATE DATABASE alx_book_store;
- 
+CREATE DATABASE IF NOT EXISTS alx_book_store;
  CREATE TABLE Authors (
     author_id INT PRIMARY KEY AUTO_INCREMENT, 
     author_name VARCHAR(215)
